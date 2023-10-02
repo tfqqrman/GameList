e5da1f8cb75579a6238039e7168949005c62ad99
tfqqrman <tfqqrman@gmail.com>
2023-10-02 21:17:11 +0700

    Initial Commit

---
+++ GameList/GameListApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameListApp.swift
+//  GameList
+//
+//  Created by Taufiq Qurohman on 02/10/23.
+//
+
+import SwiftUI
+
+@main
+struct GameListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
