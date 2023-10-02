a908d39ab3087483d1be1268a25b204b6b4ff0e3
tfqqrman <tfqqrman@gmail.com>
2023-10-02 22:11:43 +0700

    Adding splash screen

---
+++ GameList/Views/HomeScreenView.swift
@@ -0,0 +1,20 @@
+//
+//  HomeScreenView.swift
+//  GameList
+//
+//  Created by Taufiq Qurohman on 02/10/23.
+//
+
+import SwiftUI
+
+struct HomeScreenView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HomeScreenView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeScreenView()
+    }
+}
